2e687489d574e28afd9d393ddc3d8ccb614ff1d0
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2023-10-06 03:34:51 +0600

    Create Problem1.m

---
+++ Problem1.m
@@ -0,0 +1,16 @@
+x = [0:0.1:10];
+y = sin (x);
+z = cos (x);
+subplot (3,1,1);
+plot (x,y);
+grid on;
+subplot (3,1,2);
+plot (x,z);
+grid on; 
+hold on;
+subplot (3,1,3);
+stem (x,z);
+grid on;
+hold on;
+subplot (3,1,3);
+stem (x,y,'r');
